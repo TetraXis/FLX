de62384c1a937c6470203e8b402df88dd2e89c14
Черкасов Данил <danilcherlasov@gmail.com>
2025-08-10 00:32:05 +0500

    Truly fixed x86 support now

--- flx_new/include/flx_new.hpp
+++ flx_new/include/flx_new.hpp
@@ -16,10 +16,18 @@
 /// Forcing all types to implement placement new is bad
 /// MSVC doesn't have weak linking, so we need entierly unique placement new
 ///                                                                This makes this placement new unique    vvvvvvvvvvvv
+
+#if defined(_M_X64) || defined(__x86_64__) // x64
 constexpr inline void* operator new ([[maybe_unused]] unsigned long long size, void* ptr, [[maybe_unused]] bool use_flx) noexcept
 {
 	return ptr;
 }
+#else // x86
+constexpr inline void* operator new ([[maybe_unused]] unsigned int size, void* ptr, [[maybe_unused]] bool use_flx) noexcept
+{
+	return ptr;
+}
+#endif
 
 // To use this placement new we need:
 //                  vvvvvv
