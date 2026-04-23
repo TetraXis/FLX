a3e7408330d3bb5a5da4fd8af1c993b48ec216ab
Черкасов Данил <danilcherlasov@gmail.com>
2026-04-23 12:21:05 +0500

    Testing VS.

--- TEST/test/type_traits.hpp
+++ TEST/test/type_traits.hpp
@@ -25,7 +25,8 @@ namespace test
 			check(flx::is_same<volatile int, int> == std::is_same_v<volatile int, int>, "Mismatch with STD 06");
 			check(flx::is_same<volatile int, volatile int> == std::is_same_v<volatile int, volatile int>, "Mismatch with STD 07");
 			check(flx::is_same<std::string, int> == std::is_same_v<std::string, int>, "Mismatch with STD 08");
-			check(flx::is_same<std::string, std::string> == std::is_same_v<std::string, std::string>, "Mismatch with STD 09");check(flx::is_same<const volatile int, const volatile int> == std::is_same_v<const volatile int, const volatile int>, "Mismatch with STD 0A");
+			check(flx::is_same<std::string, std::string> == std::is_same_v<std::string, std::string>, "Mismatch with STD 09");
+			check(flx::is_same<const volatile int, const volatile int> == std::is_same_v<const volatile int, const volatile int>, "Mismatch with STD 0A");
 			check(flx::is_same<const volatile int, const int> == std::is_same_v<const volatile int, const int>, "Mismatch with STD 0B");
 			check(flx::is_same<const volatile int, volatile int> == std::is_same_v<const volatile int, volatile int>, "Mismatch with STD 0C");
 			check(flx::is_same<int&, int&> == std::is_same_v<int&, int&>, "Mismatch with STD 0D");
@@ -53,27 +54,27 @@ namespace test
 			subgroup = "remove_cv";
 
 			enum E { dummy };
-			check(flx::is_same<flx::remove_cv<int>, std::remove_cv_t<int>>, "Mismatch with STD 00");
-			check(flx::is_same<flx::remove_cv<const int>, std::remove_cv_t<const int>>, "Mismatch with STD 01");
-			check(flx::is_same<flx::remove_cv<volatile int>, std::remove_cv_t<volatile int>>, "Mismatch with STD 02");
-			check(flx::is_same<flx::remove_cv<const volatile int>, std::remove_cv_t<const volatile int>>, "Mismatch with STD 03");
-			check(flx::is_same<flx::remove_cv<int*>, std::remove_cv_t<int*>>, "Mismatch with STD 04");
-			check(flx::is_same<flx::remove_cv<const int*>, std::remove_cv_t<const int*>>, "Mismatch with STD 05");
-			check(flx::is_same<flx::remove_cv<volatile int*>, std::remove_cv_t<volatile int*>>, "Mismatch with STD 06");
-			check(flx::is_same<flx::remove_cv<const volatile int*>, std::remove_cv_t<const volatile int*>>, "Mismatch with STD 07");
-			check(flx::is_same<flx::remove_cv<int* const>, std::remove_cv_t<int* const>>, "Mismatch with STD 08");
-			check(flx::is_same<flx::remove_cv<int* volatile>, std::remove_cv_t<int* volatile>>, "Mismatch with STD 09");
-			check(flx::is_same<flx::remove_cv<int* const volatile>, std::remove_cv_t<int* const volatile>>, "Mismatch with STD 0A");
-			check(flx::is_same<flx::remove_cv<const int* const>, std::remove_cv_t<const int* const>>, "Mismatch with STD 0B");
-			check(flx::is_same<flx::remove_cv<const int[5]>, std::remove_cv_t<const int[5]>>, "Mismatch with STD 0C");
-			check(flx::is_same<flx::remove_cv<volatile int[5]>, std::remove_cv_t<volatile int[5]>>, "Mismatch with STD 0D");
-			check(flx::is_same<flx::remove_cv<const volatile int[5]>, std::remove_cv_t<const volatile int[5]>>, "Mismatch with STD 0E");
-			check(flx::is_same<flx::remove_cv<void>, std::remove_cv_t<void>>, "Mismatch with STD 0F");
-			check(flx::is_same<flx::remove_cv<const void>, std::remove_cv_t<const void>>, "Mismatch with STD 10");
-			check(flx::is_same<flx::remove_cv<int&>, std::remove_cv_t<int&>>, "Mismatch with STD 11");
-			check(flx::is_same<flx::remove_cv<const int&>, std::remove_cv_t<const int&>>, "Mismatch with STD 12");
-			check(flx::is_same<flx::remove_cv<int&&>, std::remove_cv_t<int&&>>, "Mismatch with STD 13");
-			check(flx::is_same<flx::remove_cv<enum E>, std::remove_cv_t<enum E>>, "Mismatch with STD 14");
+			check(std::is_same_v<flx::remove_cv<int>, std::remove_cv_t<int>>, "Mismatch with STD 00");
+			check(std::is_same_v<flx::remove_cv<const int>, std::remove_cv_t<const int>>, "Mismatch with STD 01");
+			check(std::is_same_v<flx::remove_cv<volatile int>, std::remove_cv_t<volatile int>>, "Mismatch with STD 02");
+			check(std::is_same_v<flx::remove_cv<const volatile int>, std::remove_cv_t<const volatile int>>, "Mismatch with STD 03");
+			check(std::is_same_v<flx::remove_cv<int*>, std::remove_cv_t<int*>>, "Mismatch with STD 04");
+			check(std::is_same_v<flx::remove_cv<const int*>, std::remove_cv_t<const int*>>, "Mismatch with STD 05");
+			check(std::is_same_v<flx::remove_cv<volatile int*>, std::remove_cv_t<volatile int*>>, "Mismatch with STD 06");
+			check(std::is_same_v<flx::remove_cv<const volatile int*>, std::remove_cv_t<const volatile int*>>, "Mismatch with STD 07");
+			check(std::is_same_v<flx::remove_cv<int* const>, std::remove_cv_t<int* const>>, "Mismatch with STD 08");
+			check(std::is_same_v<flx::remove_cv<int* volatile>, std::remove_cv_t<int* volatile>>, "Mismatch with STD 09");
+			check(std::is_same_v<flx::remove_cv<int* const volatile>, std::remove_cv_t<int* const volatile>>, "Mismatch with STD 0A");
+			check(std::is_same_v<flx::remove_cv<const int* const>, std::remove_cv_t<const int* const>>, "Mismatch with STD 0B");
+			check(std::is_same_v<flx::remove_cv<const int[5]>, std::remove_cv_t<const int[5]>>, "Mismatch with STD 0C");
+			check(std::is_same_v<flx::remove_cv<volatile int[5]>, std::remove_cv_t<volatile int[5]>>, "Mismatch with STD 0D");
+			check(std::is_same_v<flx::remove_cv<const volatile int[5]>, std::remove_cv_t<const volatile int[5]>>, "Mismatch with STD 0E");
+			check(std::is_same_v<flx::remove_cv<void>, std::remove_cv_t<void>>, "Mismatch with STD 0F");
+			check(std::is_same_v<flx::remove_cv<const void>, std::remove_cv_t<const void>>, "Mismatch with STD 10");
+			check(std::is_same_v<flx::remove_cv<int&>, std::remove_cv_t<int&>>, "Mismatch with STD 11");
+			check(std::is_same_v<flx::remove_cv<const int&>, std::remove_cv_t<const int&>>, "Mismatch with STD 12");
+			check(std::is_same_v<flx::remove_cv<int&&>, std::remove_cv_t<int&&>>, "Mismatch with STD 13");
+			check(std::is_same_v<flx::remove_cv<enum E>, std::remove_cv_t<enum E>>, "Mismatch with STD 14");
 
 			subgroup = "NULL";
 		}
